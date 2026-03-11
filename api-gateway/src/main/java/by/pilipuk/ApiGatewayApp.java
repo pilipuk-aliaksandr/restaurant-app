78e149f59a452a4cd1b30c1109ae59931c2f1faf
Uladzislau.Kaliaha <uladzislau.kaliaha@noogadev.com>
2026-03-11 14:14:32 +0300

    dsfs

--- api-gateway/src/main/java/by/pilipuk/ApiGatewayApp.java
+++ api-gateway/src/main/java/by/pilipuk/ApiGatewayApp.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+//comment
 public class ApiGatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApp.class, args);
